f9d12344d080f35d8072c3759a4491e14311f4c1
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-11-02 13:00:18 -0700

    made some methods

--- src/Square.java
+++ src/Square.java
@@ -11,6 +11,14 @@ import java.util.*;
  * Arrays are immutable, this means that once their size is
  * set they won't be able to change their dimension
  * afterwards
+ * <p>
+ * REFERENCES
+ * https://www.educative.io/answers/how-to-generate-random-numbers-in-java
+ * https://www.javatpoint.com/add-elements-to-array-in-java#:~:text=In%20Java%2C%20Arrays%20are%20mutable,a%20new%20element%20in%20Array.
+ * https://www.youtube.com/watch?v=k-7jJP7QFEM
+ * https://www.dcode.fr/matrix-determinant
+ * https://www.geeksforgeeks.org/print-2-d-array-matrix-java/
+ * https://www.studypug.com/algebra-help/multiplying-a-matrix-by-another-matrix#:~:text=The%20dot%20product%20is%20the,column%20of%20the%20second%20matrix.
  */
 
 public class Square {
@@ -19,27 +27,51 @@ public class Square {
         double[][] matA = new double[nxn][nxn];
 //        System.out.println();
 
-        Random rand = new Random();
+        double[][] matXX = new double[nxn][nxn];
 
-        for (int r = 0; r < nxn; r++) {
-            //fill rows of matrix
-            for (int c = 0; c < nxn; c++) {
-                //BASE CASE VALUES
-                matA[r][c] = (r * nxn) + c;
-
-                //RANDOM ASSIGNMENT
-                //fill cols of matrix in current row
-                //the range defined by rand is from 0 to .nextInt(VALUE) - 1
-//                matA[r][c] = rand.nextInt(nxn * 2 + 1);
-            }
-        }
+        initMat(nxn, matA, false);
+        initMat(nxn, matXX, true);
 
         for (int i = 0; i < nxn; i++) {
             System.out.println(Arrays.toString(matA[i]));
 //        for debug purpose
         }
+        System.out.println();
 
-        rec(nxn, matA);
+        for (int i = 0; i < nxn; i++) {
+            System.out.println(Arrays.toString(matXX[i]));
+//        for debug purpose
+        }
+
+//        rec(nxn, matA);
+    }
+
+    /**
+     * this method will look at getting a specified submatrix
+     * statically. then use this info to make a dynamic method
+     */
+    public static void subMat() {
+        //something
+    }
+
+    public static void initMat(int n, double[][] matrix, boolean randomize) {
+        Random rand = new Random();
+
+        for (int r = 0; r < n; r++) {
+            //fill rows of matrix
+            for (int c = 0; c < n; c++) {
+                if (randomize) {
+                    //this
+                    //RANDOM ASSIGNMENT
+                    //fill cols of matrix in current row
+                    //the range defined by rand is from 0 to .nextInt(VALUE) - 1
+                    matrix[r][c] = rand.nextInt(n * 2 + 1);
+                } else {
+                    //BASE CASE VALUES
+                    matrix[r][c] = (r * n) + c;
+                }
+            }
+        }
     }
 
     public static double rec(int n, double[][] matRec) {
